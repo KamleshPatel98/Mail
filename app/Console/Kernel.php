44116eb1d6ce6ba12106ac08a89ba7ad45194a7f
KamleshPatel98 <kamleshp52170@gmail.com>
2024-02-23 22:19:23 +0530

    scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function(){
             User::whereNull('email_verified_at')->delete();
-        })->hourly();
+        })->everyMinute();
     }
 
     /**
